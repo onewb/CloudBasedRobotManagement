3118182cef0514c96c05b7f01f67191f2e7dbade
Andrew Patricio <AnderrPatr01@gmail.com>
2026-03-19 11:55:43 -0700

    fix of declared variables

--- Robot_Simulator_Script.py
+++ Robot_Simulator_Script.py
@@ -1,40 +1,44 @@
 from google.cloud import pubsub_v1
-import time, json, random
+import time, json, random, threading
 
 PROJECT_ID = "cloud-based-robot-management"
 
+# Create clients
 publisher = pubsub_v1.PublisherClient()
+subscriber = pubsub_v1.SubscriberClient()
+
+# Define paths
 topic_path = publisher.topic_path(PROJECT_ID, "robot-telemetry")
-subscription_path=subscriber.subscription_path(PROJECT_ID,"commands-sub")
+subscription_path = subscriber.subscription_path(PROJECT_ID, "commands-sub")
 
 robot_id = "robot-1"
 
-# 🔹 Publish telemetry , While true , creates artifical robot id/position between 100x100/ and a status
+# 🔹 Publish telemetry
 def publish_telemetry():
     while True:
         data = {
-            "robot_id": robot_id,                                           #robot ID
-            "position": [random.randint(0,100), random.randint(0,100)],     #position []
-            "status": random.choice(["idle", "moving", "working"])          #status
+            "robot_id": robot_id,
+            "position": [random.randint(0,100), random.randint(0,100)],
+            "status": random.choice(["idle", "moving", "working"])
         }
 
-        publisher.publish(topic_path, json.dumps(data).encode("utf-8"))     #publish (pub/sub)
-        print(f"Sent: {data}")                                              #print data sent
-        time.sleep(2)                                                       #repeats every 2 seconds
+        publisher.publish(topic_path, json.dumps(data).encode("utf-8"))
+        print(f"Sent: {data}")
+        time.sleep(2)
 
-# 🔹 Receive commands , handles incoming commands from cloud
+# 🔹 Receive commands
 def callback(message):
-    print(f"Received command: {message.data.decode()}")                     #print received command
-    message.ack()                                                          #acknowledge command (pub/sub)
+    print(f"Received command: {message.data.decode()}")
+    message.ack()
 
-# Listens for commands in infinite loop, 10 seconds,  
+# 🔹 Listen for commands
 def listen_for_commands():
-    subscriber.subscribe(subscription_path, callback=callback)      # connects to a pub/sub subscritpion to run callback() whenever message recieved
+    subscriber.subscribe(subscription_path, callback=callback)
     print("Listening for commands...")
     while True:
         time.sleep(10)
 
-# 🔹 Run both in parallel as threads
+# 🔹 Run both in parallel
 if __name__ == "__main__":
     threading.Thread(target=publish_telemetry).start()
     threading.Thread(target=listen_for_commands).start()
